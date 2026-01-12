051b958a5d3d489bba473138cbc41b872e0b1907
deepanshukapoor192 <deepanshukapoor192@gmail.com>
2026-01-13 01:18:52 +0530

    asdfg

--- chess-frontend-service/src/Swap_chess.js
+++ chess-frontend-service/src/Swap_chess.js
@@ -7,7 +7,7 @@ function Swap_chess() {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Swap Chess</Navbar.Brand>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
         </Container>
       </Navbar>
       
