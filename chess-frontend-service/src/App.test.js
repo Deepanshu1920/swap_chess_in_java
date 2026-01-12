a1d19d5fb42eecee44040d9824bd9134856a27aa
deepanshukapoor192 <deepanshukapoor192@gmail.com>
2026-01-13 01:13:47 +0530

    home page

--- chess-frontend-service/src/App.test.js
+++ chess-frontend-service/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './Swap_chess';
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ chess-frontend-service/src/Swap_chess.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './Swap_chess.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+function Swap_chess() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Swap Chess</Navbar.Brand>
+        </Container>
+      </Navbar>
+      
+      <br />
+      
+     
+      
+      
+    </>
+  );
+}
+
+export default Swap_chess;
